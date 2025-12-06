53570528c46a0783c0e0174b8ff6938ad6a07754
abhiram79 <abhiramv670@gmail.com>
2025-12-06 18:44:23 +0530

    Removed creating new user

--- debian.sh
+++ debian.sh
@@ -21,34 +21,19 @@ apt update -y && apt upgrade -y
 
 echo 'Installing sudo and utils...'
 apt install -y sudo passwd
-
-echo 'Creating user: debian'
-useradd -m -s /bin/bash debian
-
-echo 'Setting password to: debian'
-echo 'debian:debian' | chpasswd
-
-echo 'Adding user to sudoers...'
-usermod -aG sudo debian
-
-# Ensure password-based sudo works
-sed -i 's/# %sudo/%sudo/' /etc/sudoers
-
 echo 'Done configuring Debian.'
 "
-
+clear
 echo ""
 echo "=============================="
 echo " Debian Installed Successfully!"
-echo " User: debian"
-echo " Password: debian"
 echo "=============================="
 echo ""
 
 echo "Creating shortcut launcher: debian"
 cat > $PREFIX/bin/debian << 'EOF'
 #!/data/data/com.termux/files/usr/bin/bash
-proot-distro login debian --user debian
+proot-distro login debian
 EOF
 
 chmod +x $PREFIX/bin/debian
