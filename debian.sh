04e74d942899590878f3c3a5d30d0dc18953462f
abhiram79 <abhiramv670@gmail.com>
2025-12-06 18:52:36 +0530

    add custom colored PS1 prompt to .bashrc

--- debian.sh
+++ debian.sh
@@ -19,6 +19,7 @@ set -e
 echo 'Updating Debian inside PRoot...'
 apt update -y && apt upgrade -y
 
+# add Custom colours 
 echo "" >> ~/.bashrc
 echo "# Custom colored prompt" >> ~/.bashrc
 echo "PS1='\\[\\e[1;31m\\]\\u\\[\\e[0m\\]@\\[\\e[1;32m\\]\\h\\[\\e[0m\\]:\\[\\e[1;36m\\]\\w\\[\\e[0m\\]\\$ '" >> ~/.bashrc
